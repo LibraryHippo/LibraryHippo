e57004f01cc20a62a9b0b1773545747b7827e5cb
Blair Conrad <blair.conrad@gmail.com>
2013-03-24 08:03:25 -0400

    use powershell instead of cmd when working on LibraryHippo

--- dev.bat
+++ dev.bat
@@ -1,2 +1,2 @@
-start "LH dev server" /D%~p0\App cmd /C ..\..\go.bat .
-start "LH working dir" /D%~p0 cmd
+start "LH dev server" /D%~p0\App cmd /C powershell -NoExit dev_appserver.py --show_mail_body .
+start "LH working dir" /D%~p0 cmd /C powershell -NoExit
