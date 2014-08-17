5e7b143341466954842020bc8660643982177e22
Blair Conrad <blair@blairconrad.com>
2014-08-16 22:06:23 -0400

    added serve.bat to run the dev server

--- dev.bat
+++ dev.bat
@@ -1,2 +1,2 @@
-start "LH dev server" /D%~p0\App cmd /C powershell -NoExit dev_appserver.py --show_mail_body yes .
+start "LH dev server" /D%~p0 cmd /C powershell -NoExit .\serve.bat
 start "LH working dir" /D%~p0 cmd /C powershell -NoExit
