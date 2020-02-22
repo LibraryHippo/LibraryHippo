ed24da26018e0d68d585ebba7cbe4a94b5a8748f
Blair Conrad <blair@blairconrad.com>
2020-02-22 12:00:52 -0500

    Configure loggers

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,48 @@
-from config import Config
+from logging.config import dictConfig
+import os
 
 from flask import Flask
 from flask_mail import Mail
+
+from config import Config
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+                "level": "INFO",
+            },
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": "logs/libraryhippo.log",
+                "maxBytes": 1024000,
+                "backupCount": 10,
+                "formatter": "default",
+                "level": "DEBUG",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["wsgi", "file"]},
+    }
+)
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 mail = Mail(app)
+
+app.logger.info("LibraryHippo startup")
 
 from app import routes
