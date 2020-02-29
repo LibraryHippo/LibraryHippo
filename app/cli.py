f61b1e0e33893000abbd4a974bfc9ddeb17f9a98
Blair Conrad <blair@blairconrad.com>
2020-02-29 06:21:42 -0500

    Send last card state in e-mail

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,7 @@
+import json
 import time
 
-from app import mail
+from app import mail, models
 from datetime import datetime
 from flask_mail import Message
 
@@ -8,15 +9,31 @@ from flask_mail import Message
 def register(app):
     @app.cli.command("notify-all")
     def notify_all():
+        card = models.Card.query.get(1)  # a hack - we know there's only 1 card for now
+        last_card_state = json.loads(card.last_state)
+
+        html_body = "<h1>Holds</h1>"
+        for hold in last_card_state["holds"]:
+            html_body += "<dl>"
+            for k, v in hold.items():
+                html_body += f"<dt>{k}</dt><dd>{v}</dd>"
+            html_body += "</dl><hr>"
+
+        html_body += "<h1>Checkouts</h1>"
+        for checkout in last_card_state["checkouts"]:
+            html_body += "<dl>"
+            for k, v in checkout.items():
+                html_body += f"<dt>{k}</dt><dd>{v}</dd>"
+            html_body += "</dl><hr>"
+
         now = datetime.now().isoformat()
 
         msg = Message(
             "LibraryHippo starting notifications", recipients=["blair@blairconrad.com"]
         )
         msg.body = f"starting notifications at {now}"
-        msg.html = f"<h1>Test mail from LibraryHippo</h1><p>{msg.body}."
+        msg.html = html_body
 
-        print(msg.body)
         mail.send(msg)
 
         time.sleep(300)
@@ -27,7 +44,6 @@ def register(app):
             "LibraryHippo ending notifications", recipients=["blair@blairconrad.com"]
         )
         msg.body = f"ending notifications at {now}"
-        msg.html = f"<h1>Test mail from LibraryHippo</h1><p>{msg.body}."
+        msg.html = html_body
 
-        print(msg.body)
         mail.send(msg)
