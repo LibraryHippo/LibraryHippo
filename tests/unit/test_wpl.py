f867e2dba8edb6808d631dc0d5a1e8b0c07a93b3
Blair Conrad <blair@blairconrad.com>
2020-05-14 22:08:42 -0400

    Mock requests in unit test

--- tests/unit/test_wpl.py
+++ tests/unit/test_wpl.py
@@ -2,14 +2,40 @@ from app.libraries.wpl import WPL
 from app.models import Card
 
 
-def test_check_card_finds_holds():
+def test_check_card_finds_holds(requests_mock):
+    login_url = (
+        "https://books.kpl.org/iii/cas/login?service="
+        + "https://books.kpl.org/patroninfo~S3/"
+        + "j_acegi_cas_security_check&lang=eng&scope=3"
+    )
+
+    requests_mock.get(login_url, text="")
+    requests_mock.post(login_url, text="<a href='/holds'>holds</a>")
+
+    requests_mock.get(
+        "/holds",
+        text="""
+             <table class="patFunc">
+             <tr class="patFuncHeaders"><th> TITLE </th><th>STATUS</th></tr>
+             <tr class="patFuncEntry">
+                 <td class="patFuncTitle">Blood heir / Amélie Wen Zhao</td>
+                 <td class="patFuncStatus"> 9 of 83 holds </td>
+             </tr>
+             </table>
+             """,
+    )
+
     card = make_card()
 
     target = WPL()
     check_result = target.check_card(card)
 
-    assert check_result is not None
+    assert check_result
     assert check_result["holds"]
+    assert check_result["holds"][0] == {
+        "Status": " 9 of 83 holds ",
+        "Title": "Blood heir / Amélie Wen Zhao",
+    }
 
 
 def make_card():
